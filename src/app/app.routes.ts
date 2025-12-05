c37420e9f6de4e8f84f343edf9aef8b2f2e4f26a
neriiman <neriman@live.se>
2025-12-05 17:18:08 +0100

    links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { TimerComponent } from './pages/timer/timer.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: TimerComponent,
+    title: 'timer',
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent,
+    title: 'your tasks',
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    title: 'settings',
+  },
+];
--- src/app/components/tab-bar/tab-bar.component.ts
+++ src/app/components/tab-bar/tab-bar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-tab-bar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './tab-bar.component.html',
   styleUrl: './tab-bar.component.scss',
 })
