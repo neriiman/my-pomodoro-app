33106f63f693ad9633e35f0e9ac87cae8d1f0d0f
neriiman <neriman@live.se>
2025-12-05 15:17:49 +0100

    created some reusable components and pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TabBarComponent } from "./components/tab-bar/tab-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TabBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
