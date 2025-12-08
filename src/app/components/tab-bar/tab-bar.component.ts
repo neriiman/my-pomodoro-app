478c4bd3fd33f6df5708fa8b9380f55687393788
neriiman <neriman@live.se>
2025-12-08 15:26:40 +0100

    navigation bar

--- src/app/components/tab-bar/tab-bar.component.ts
+++ src/app/components/tab-bar/tab-bar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-tab-bar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './tab-bar.component.html',
   styleUrl: './tab-bar.component.scss',
 })
 export class TabBarComponent {
-
+  navItems = signal([
+    {
+      path: '/',
+      tab: 'Timer',
+      exact: true,
+    },
+    {
+      path: '/tasks',
+      tab: 'Tasks',
+      exact: false,
+    },
+    {
+      path: '/settings',
+      tab: 'Settings',
+      exact: false,
+    },
+  ]);
 }
