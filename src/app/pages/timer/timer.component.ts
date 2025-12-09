5f8bc408c90ccff133de83b5c454bf63bb376e67
neriiman <neriman@live.se>
2025-12-09 17:07:59 +0100

    working on timer component

--- src/app/pages/timer/timer.component.ts
+++ src/app/pages/timer/timer.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-timer',
-  imports: [],
+  imports: [MatProgressSpinner, MatIconModule],
   templateUrl: './timer.component.html',
   styleUrl: './timer.component.scss',
 })
 export class TimerComponent {
-
+  activeButton = signal('Focus');
+  buttons = signal([
+    {
+      label: 'Focus',
+      icon: 'laptop-windows',
+    },
+    {
+      label: 'Short break',
+      icon: 'self_improvement',
+    },
+    {
+      label: 'Long break',
+      icon: 'self_improvement',
+    },
+  ]);
+  setActiveButton(label: string) {
+    this.activeButton.set(label);
+  }
 }
