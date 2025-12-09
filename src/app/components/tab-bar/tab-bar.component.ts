8e592dcb596d8dde8f582c960555d2ffa5f39614
neriiman <neriman@live.se>
2025-12-09 13:21:40 +0100

    material icons

--- src/app/components/tab-bar/tab-bar.component.ts
+++ src/app/components/tab-bar/tab-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-tab-bar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './tab-bar.component.html',
   styleUrl: './tab-bar.component.scss',
 })
@@ -11,18 +12,24 @@ export class TabBarComponent {
   navItems = signal([
     {
       path: '/',
-      tab: 'Timer',
+      tab: 'Focus',
       exact: true,
+      ariaLabel: '',
+      icon: 'timer',
     },
     {
       path: '/tasks',
       tab: 'Tasks',
       exact: false,
+      ariaLabel: 'assignments',
+      icon: 'assignment',
     },
     {
       path: '/settings',
       tab: 'Settings',
       exact: false,
+      ariaLabel: 'settings',
+      icon: 'settings',
     },
   ]);
 }
