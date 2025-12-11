f404ff2432625725a55cb072da849a6f86484229
neriiman <neriman@live.se>
2025-12-11 17:36:29 +0100

    worked on timer component and button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
-
+  classList = input<string>();
+  hasIcon = input<boolean>(false);
+  label = input.required<string>();
+  hierarchy = input<'primary' | 'secondary'>('secondary');
+  iconName = input<string>('');
+  shadowed = input<boolean>(false)
 }
--- src/app/pages/timer/timer.component.ts
+++ src/app/pages/timer/timer.component.ts
@@ -1,16 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { ButtonComponent } from '../../components/button/button.component';
+
+interface ControlButton {
+  label: 'Start' | 'Pause' | 'Reset';
+  icon: string;
+  hierarchy: 'primary' | 'secondary';
+}
+
+interface TimerButton {
+  label: 'Focus' | 'Short break' | 'Long break';
+  icon: string;
+}
 
 @Component({
   selector: 'app-timer',
-  imports: [MatProgressSpinner, MatIconModule],
+  imports: [MatProgressSpinner, MatIconModule, ButtonComponent],
   templateUrl: './timer.component.html',
   styleUrl: './timer.component.scss',
 })
 export class TimerComponent {
   activeButton = signal('Focus');
-  buttons = signal([
+  timerIsStarted = signal(false);
+  timerButtons = signal<TimerButton[]>([
     {
       label: 'Focus',
       icon: 'laptop-windows',
@@ -24,7 +37,20 @@ export class TimerComponent {
       icon: 'self_improvement',
     },
   ]);
-  setActiveButton(label: string) {
+  controlButtons = signal<ControlButton[]>([
+    {
+      label: this.timerIsStarted() ? 'Pause' : 'Start',
+      icon: this.timerIsStarted() ? 'timer_pause' : 'timer_play',
+      hierarchy: 'primary',
+    },
+    {
+      label: 'Reset',
+      icon: 'settings_backup_restore',
+      hierarchy: 'secondary',
+    },
+  ]);
+
+  setActiveTimerButton(label: string) {
     this.activeButton.set(label);
   }
 }
